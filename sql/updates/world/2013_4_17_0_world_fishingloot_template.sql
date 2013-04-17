e0aa8013499507a1b53713d14b991e54c255e251
Sayorim <telias@gmx.net>
2013-04-17 08:28:34 +0200

    add missing fishingloot for Dalaran/Shadowside

---
+++ sql/updates/world/2013_4_17_0_world_fishingloot_template.sql
@@ -0,0 +1,6 @@
+ -- Kanalratte
+ INSERT INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES ('4395', '43698', '5');
+-- Kanalisationskarpfen
+ INSERT INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES ('4395', '43571', '100');
+ -- Magiesauger
+ INSERT INTO `fishing_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES ('4395', '43572', '100');
